7057aa18af63d1f1444585dbf16131f85815c5a1
Karan Bhatt <43069315+karanbhatt108@users.noreply.github.com>
2019-08-26 18:40:37 -0400

    Add files via upload

---
+++ Default.aspx.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Template
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            /* Purpose: This page is used only to redirect to the Login page.*/
+            Response.Redirect("secure");
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CIS4396_Template_SSO.Global" Language="C#" %>
